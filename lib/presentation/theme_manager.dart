96254de8ba5623f828439cbdb563cc67ba6061e0
Ahmed Tarek <at4186815@gmail.com>
2023-08-16 18:53:46 +0300

    add main color theme

---
+++ lib/presentation/theme_manager.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/color_manager.dart';
+
+ThemeData getAppTheme() {
+  return ThemeData(
+    // main colors
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.lightPrimary,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+
+  );
+}
