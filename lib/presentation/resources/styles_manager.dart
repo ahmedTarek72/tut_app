53bcbcf43b383eb75bbc4521d8239ce8279a18e8
Ahmed Tarek <at4186815@gmail.com>
2023-08-17 16:43:51 +0300

    add routes manager part 1

--- lib/presentation/resources/styles_manager.dart
+++ lib/presentation/resources/styles_manager.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tut_app/presentation/font_manager.dart';
+import 'font_manager.dart';
 
 TextStyle _getTextStyle(
     {required double fontSize,
