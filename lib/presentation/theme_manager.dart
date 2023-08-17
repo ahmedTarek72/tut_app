f6dd2e1817db7c163736ef7b70fca6877e28b82e
Ahmed Tarek <at4186815@gmail.com>
2023-08-17 15:33:19 +0300

    add AppBar THeme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tut_app/presentation/color_manager.dart';
+import 'package:tut_app/presentation/font_manager.dart';
+import 'package:tut_app/presentation/styles_manager.dart';
 import 'package:tut_app/presentation/values_manager.dart';
 
 ThemeData getAppTheme() {
@@ -9,11 +11,20 @@ ThemeData getAppTheme() {
     primaryColorLight: ColorManager.lightPrimary,
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.lightPrimary,
     // card theme
     cardTheme: CardTheme(
       color: ColorManager.white,
       elevation: AppSize.s4,
       shadowColor: ColorManager.grey,
     ),
+
+    // AppBar THeme
+    appBarTheme: AppBarTheme(
+        centerTitle: true,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.primary,
+        titleTextStyle:
+            getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16)),
   );
 }
